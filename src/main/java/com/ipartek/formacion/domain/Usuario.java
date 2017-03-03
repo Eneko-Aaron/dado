df5aed43fb2869645bbaaf95301f5b6b468b3ced
AarondelRio <aaronriosainz@gmail.com>
2017-03-03 13:30:20 +0100

    faltan fechas en Usuario y get All usuarioDAO

--- src/main/java/com/ipartek/formacion/domain/Usuario.java
+++ src/main/java/com/ipartek/formacion/domain/Usuario.java
@@ -7,9 +7,9 @@ public class Usuario {
 	private int id;
 	private String nombre;
 	
-	private Date fecha_alta;
-	private	Date fecha_baja;
-	private Date fecha_modificacion;
+	private Date fechaAlta;
+	private	Date fechaBaja;
+	private Date fechaModificacion;
 	
 	private ArrayList<Dado> tiradas;
 	private int numTiradas;
@@ -44,32 +44,32 @@ public class Usuario {
 
 
 	public Date getFecha_alta() {
-		return fecha_alta;
+		return fechaAlta;
 	}
 
 
 	public void setFecha_alta(Date fecha_alta) {
-		this.fecha_alta = fecha_alta;
+		this.fechaAlta = fecha_alta;
 	}
 
 
 	public Date getFecha_baja() {
-		return fecha_baja;
+		return fechaBaja;
 	}
 
 
 	public void setFecha_baja(Date fecha_baja) {
-		this.fecha_baja = fecha_baja;
+		this.fechaBaja = fecha_baja;
 	}
 
 
 	public Date getFecha_modificacion() {
-		return fecha_modificacion;
+		return fechaModificacion;
 	}
 
 
 	public void setFecha_modificacion(Date fecha_modificacion) {
-		this.fecha_modificacion = fecha_modificacion;
+		this.fechaModificacion = fecha_modificacion;
 	}
 
 	public ArrayList<Dado> getTiradas() {
@@ -91,8 +91,8 @@ public class Usuario {
 	//toString
 	@Override
 	public String toString() {
-		return "Usuario [id=" + id + ", nombre=" + nombre + ", fecha_alta=" + fecha_alta + ", fecha_baja=" + fecha_baja
-				+ ", fecha_modificacion=" + fecha_modificacion + ", tiradas=" + tiradas + ", numTiradas=" + numTiradas
+		return "Usuario [id=" + id + ", nombre=" + nombre + ", fecha_alta=" + fechaAlta + ", fecha_baja=" + fechaBaja
+				+ ", fecha_modificacion=" + fechaModificacion + ", tiradas=" + tiradas + ", numTiradas=" + numTiradas
 				+ "]";
 	}
 
